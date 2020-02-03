c825000936d4eb7e3a2e06d932c15aa0858fd132
jgrando <john.grando@gmail.com>
2020-02-02 22:26:57 -0500

    continued work

--- ch02.scala
+++ ch02.scala
@@ -44,5 +44,62 @@ val matchSummary = matches.describe()
 val misses = parsed.filter($"is_match" === false)
 val missSummary = misses.describe()
 
+// load Pivot sript
+:load Pivot.scala
 
+// Create variables for matches and miss_desc
+val matchSummaryT = pivotSummary(matchSummary)
+val missSummaryT = pivotSummary(missSummary)
 
+// Test join
+matchSummaryT.createOrReplaceTempView("match_desc")
+missSummaryT.createOrReplaceTempView("miss_desc")
+spark.sql("""
+  SELECT a.field, a.count + b.count total, a.mean - b.mean delta
+  FROM match_desc a INNER JOIN miss_desc b ON a.field = b.field
+  WHERE a.field NOT IN ("id_1", "id_2")
+  ORDER BY delta DESC, total DESC
+""").show()
+
+// Create case class
+case class MatchData(
+  id_1: Int,
+  id_2: Int,
+  cmp_fname_c1: Option[Double],
+  cmp_fname_c2: Option[Double],
+  cmp_lname_c1: Option[Double],
+  cmp_lname_c2: Option[Double],
+  cmp_sex: Option[Int],
+  cmp_bd: Option[Int],
+  cmp_bm: Option[Int],
+  cmp_by: Option[Int],
+  cmp_plz: Option[Int],
+  is_match: Boolean
+)
+
+// Create schema, not in book (workaround)
+import org.apache.spark.sql.types.StructType
+val matchSchema = new StructType()
+  .add("id_1", "int")
+  .add("id_2", "int")
+  .add("cmp_fname_c1", "double")
+  .add("cmp_fname_c2", "double")
+  .add("cmp_lname_c1", "double")
+  .add("cmp_lname_c2", "double")
+  .add("cmp_sex", "int")
+  .add("cmp_bd", "int")
+  .add("cmp_bm", "int")
+  .add("cmp_by", "int")
+  .add("cmp_plz", "int")
+  .add("is_match", "boolean")
+
+// Reload data with schema
+val parsed = spark.read.
+  option("header", "true").
+  option("nullValue", "?").
+  schema(matchSchema).
+  csv("linkage")
+
+// Convert DataFrame to Dataset
+val matchData = parsed.as[MatchData]
+matchData.show()
