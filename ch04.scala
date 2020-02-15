f40a9a24b8fc927a53278cd520b3ce87e074baaf
John Grando <grandocu@gmail.com>
2020-02-14 23:07:37 -0500

    working on ch04

--- ch04.scala
+++ ch04.scala
@@ -107,3 +107,103 @@ val testPriorProbabilities = classProbabilities(testData)
 trainPriorProbabilities.zip(testPriorProbabilities).map {
   case (trainProb, cvProb) => trainProb * cvProb
 }.sum
+
+// Create pipeline for combinations of hyperparameters to be tested
+import org.apache.spark.ml.Pipeline
+val inputCols = trainData.columns.filter(_ != "Cover_Type")
+val assembler = new VectorAssembler().
+  setInputCols(inputCols).
+  setOutputCol("featureVector")
+val classifier = new DecisionTreeClassifier().
+  setSeed(Random.nextLong()).
+  setLabelCol("Cover_Type").
+  setFeaturesCol("featureVector").
+  setPredictionCol("prediction")
+val pipeline = new Pipeline().setStages(Array(assembler, classifier))
+
+// Use ParamGridBuilder to set up testing grid
+import org.apache.spark.ml.tuning.ParamGridBuilder
+val paramGrid = new ParamGridBuilder().
+  addGrid(classifier.impurity, Seq("gini", "entropy")).
+  addGrid(classifier.maxDepth, Seq(1, 20)).
+  addGrid(classifier.maxBins, Seq(40, 300)).
+  addGrid(classifier.minInfoGain, Seq(0.0, 0.05)).
+  build()
+
+// Create Evaluator to define label and metric used
+val multiclassEval = new MulticlassClassificationEvaluator().
+  setLabelCol("Cover_Type").
+  setPredictionCol("prediction").
+  setMetricName("accuracy")
+
+// Split data to train/validation sets and run hyper parameter training
+import org.apache.spark.ml.tuning.TrainValidationSplit
+val validator = new TrainValidationSplit().
+  setSeed(Random.nextLong()).
+  setEstimator(pipeline).
+  setEvaluator(multiclassEval).
+  setEstimatorParamMaps(paramGrid).
+  setTrainRatio(0.9)
+
+  // Repartition trainData due to its size and available resrouces
+  //val trainDataPartitioned = trainData.repartition(100)
+
+//val validatorModel = validator.fit(trainDataPartitioned)
+
+// Print best model  Commented because grid search also commented out to
+// save time
+
+// import org.apache.spark.ml.PipelineModel
+// val bestModel = validatorModel.bestModel
+// bestModel.asInstanceOf[PipelineModel].stages.last.extractParamMap
+
+// Ouptut printed here so grid doesn't have to be run again
+//{
+//	dtc_066265adec3f-cacheNodeIds: false,
+//	dtc_066265adec3f-checkpointInterval: 10,
+//	dtc_066265adec3f-featuresCol: featureVector,
+//	dtc_066265adec3f-impurity: entropy,
+//	dtc_066265adec3f-labelCol: Cover_Type,
+//	dtc_066265adec3f-maxBins: 300,
+//	dtc_066265adec3f-maxDepth: 20,
+//	dtc_066265adec3f-maxMemoryInMB: 256,
+//	dtc_066265adec3f-minInfoGain: 0.0,
+//	dtc_066265adec3f-minInstancesPerNode: 1,
+//	dtc_066265adec3f-predictionCol: prediction,
+//	dtc_066265adec3f-probabilityCol: probability,
+//	dtc_066265adec3f-rawPredictionCol: rawPrediction,
+//	dtc_066265adec3f-seed: -8253560235143534512
+//}
+
+// Show results from all models
+//val validatorModel = validator.fit(trainData)
+//val paramsAndMetrics = validatorModel.validationMetrics.
+//  zip(validatorModel.getEstimatorParamMaps).sortBy(-_._1)
+//paramsAndMetrics.foreach { case (metric, params) =>
+//    println(metric)
+//    println(params)
+//    println()
+//}
+
+// Run best model on test test and compare accuracy
+//validatorModel.validationMetrics.max
+//multiclassEval.evaluate(bestModel.transform(testData))
+
+// Make model based on grid results so it doesn't have to be run each time
+
+val bestClassifier = new DecisionTreeClassifier().
+  setSeed(Random.nextLong()).
+  setLabelCol("Cover_Type").
+  setFeaturesCol("featureVector").
+  setPredictionCol("prediction").
+  setImpurity("entropy").
+  setMaxDepth(20).
+  setMaxBins(300).
+  setMinInfoGain(0.0)
+
+val bestPipeline = new Pipeline().setStages(Array(assembler, bestClassifier))
+val bestModelShort = bestPipeline.fit(trainData)
+
+// Show train and test data metrics
+multiclassEval.evaluate(bestModelShort.transform(trainData))
+multiclassEval.evaluate(bestModelShort.transform(testData))
